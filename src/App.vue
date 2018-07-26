d2095a0c1ef7f75bd6ad44303b4078cb1da758e6
吕任 <lvren@kingsoft.com>
2018-07-26 17:36:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app" style="height:100%;">
+    <view-box ref="viewBox">
+      <router-view></router-view>
+      <tabbar slot="bottom">adsfasdf</tabbar>
+    </view-box>
+  </div>
+</template>
+
+<script>
+import { ViewBox, Tabbar } from "vux";
+
+export default {
+  components: {
+    ViewBox,
+    Tabbar
+  },
+  name: "app"
+};
+</script>
+
+<style lang="less">
+@import "~vux/src/styles/reset.less";
+
+body {
+  background-color: #fbf9fe;
+}
+</style>
