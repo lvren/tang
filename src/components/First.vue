5b7355b4b7b22065429209ebba5009867a671759
lvren <lvren@bytedance.com>
2018-10-08 17:09:33 +0800

    fix 修改首页文案

--- src/components/First.vue
+++ src/components/First.vue
@@ -4,38 +4,27 @@
       <card>
         <img slot="header" src="../assets/background.jpeg" style="width:100%;display:block;">
         <div slot="content">
-          <div class="desc-title">
-            <img slot="header" src="../assets/person.jpeg" class="title-img" />
-            <div class="title-desc">
-              <span class="title-main">郑茹丹</span>
-              <span>本期校友</span>
-            </div>
-            <div class="title-sec">丹麦留学</div>
-          </div>
-          <div class="detail-content">
-            <div class="detail-title">校友简介</div>
+          <div class="detail-content" style="padding-top: 12px">
+            <div class="detail-title">“校友说”简介</div>
             <div class="detail-main">
-              <p>郑茹丹，来自云南昆明，2013年来到丹麦求学。茹丹目前就读于丹麦奥尔堡大学全球化文化交流专业，是一名二年级研究生。</p>
-              <p>茹丹的求学之路始于丹麦西南商学院市场管理专业（AP项目=国内的专科）。从丹麦西南商学院毕业后，又顺利申请到北丹麦大学学院的国际销售和管理专业（BA项目=国内的本科），以及现在的奥尔堡大学全球化文化交流专业。（MA项目=国内的研究生）。</p>
-              <p>来丹麦留学的五年里，茹丹跨越了三个主流丹麦高校类型（商学院、大学学院和综合性大学），学到了一些和国内不一样的学习生活经历。她期待把自己的经历和对丹麦感兴趣的学生分享，为他们精彩的留学梦做好准备。</p>
-            </div>
-          </div>
-          <div class="detail-content">
-            <div class="detail-title">分享主题（约一个小时）</div>
-            <div class="detail-main">
+              <p>“校友说”项目致力于链接你和国外高校在读校友，让校友带你走“近”梦想校园。我们希望打破留学信息不对称的壁垒，由在读校友来介绍自己的学校，帮助申请学生，而不是中介。</p>
+              <p>“校友说”项目由原丹麦领馆高等教育官员发起，首期服务于希望2019年入读丹麦高校的学生。</p>
+              <p style="font-weight: bolder;text-decoration: underline;">现在，只要1元，你就可以在2018年申请季：</p>
               <ul>
-                <li>丹麦学校</li>
-                <li>学习环境</li>
-                <li>专业选择</li>
-                <li>学校申请</li>
-                <li>留学生活常识（住宿）</li>
+                <li>由资深丹麦高等教育官员陪你唠“丹麦留学那些事”</li>
+                <li>收到丹麦留学干货/申请季动态分享</li>
+                <li>聆听每周丹麦高校在读校友在线分享/答疑</li>
+                <li>结识志同道合的留学小伙伴</li>
+                <li>选择更多的申请季增值服务(包括但不限于由丹麦高校在读校友提供的一对一咨询、文书修改以及伴申服务等)</li>
               </ul>
             </div>
           </div>
           <div class="detail-content">
-            <div class="detail-title">分享时间</div>
-            <div class="detail-main" style="padding-bottom: 12px">
-              2018.9.22日晚上8点
+            <div class="detail-main" style="font-weight: bolder;text-decoration: underline;">1元即刻进群、开启丹麦留学申请季梦想之旅！</div>
+          </div>
+          <div class="detail-content">
+            <div class="detail-main" style="padding: 12px 0;margin-left: -6px;">
+              <check-icon :value.sync="agreement"> 我已阅读并同意<span @click="handleAgreement">《校友说服务协议》</span></check-icon>
             </div>
           </div>
         </div>
@@ -46,6 +35,91 @@
     </div>
     <div v-transfer-dom>
       <loading :show="showLoading"></loading>
+    </div>
+    <div v-transfer-dom>
+      <x-dialog v-model="showAgreement" class="agreement-list">
+        <p class="dialog-title">用户协议</p>
+        <div class="img-box agreement-content" style="">
+          <div class="content-title">一、总则</div>
+          <div class="content-detail">1. 校友说的所有权和运营权归上海逐境网络科技有限公司所有。</div>
+          <div class="content-detail">2. 用户在注册之前，应当仔细阅读本协议，并同意遵守本协议后方可成为注册用户。一旦注册成功，则用户与校友说之间自动形成协议关系，用户应当受本协议的约束。</div>
+          <div class="content-detail">3. 本协议可由校友说随时更新，用户应当及时关注并同意校友说不承担通知义务。</div>
+            
+          <div class="content-title">二、服务内容</div>
+          <div class="content-detail">2.1 校友说首期服务内容包括：提供2018年申请季丹麦留学咨询服务（包括基本服务和增值服务），具体服务内容根据实际情况提供和收费。校友说可能会根据本服务的整体规划，对本服务相关权益细则、收费标准、方式等进行修改和变更，前述修改、变更，校友说将在相应服务页面进行展示。</div>
+
+          <div class="content-title">三、服务期限</div>
+          <div class="content-detail">3.1 基本服务提供时间为2018年10月1日至2019年2月1日，增值服务提供时间为2018年10月1日至2019年8月31日。校友说可能会根据本服务的整体规划和实际情况，对服务期限变更（缩短或者延长）。</div>
+
+          <div class="content-title">四、关于收费</div>
+          <div class="content-detail">4.1 本服务是校友说提供的收费服务，您须经注册系统完成注册程序并按照本服务收费标准支付相应费用后，方可使用本服务，具体服务内容根据实际情况提供和收费。</div>
+
+          <div class="content-title">五、关于支付</div>
+          <div class="content-detail">5.1 您应该使用微信支付方式依约支付。支付完成后，不可进行转让或退订。</div>
+          <div class="content-detail">
+            5.2 您不得通过以下任何方式为自己或他人开通本服务：
+            <ul>
+              <li>（1）以营利、经营等非个人使用的目的为自己或他人开通本服务</li>
+              <li>（2）通过任何机器人软件、蜘蛛软件、爬虫软件、刷屏软件等任何程序、软件方式为自己或他人开通本服务</li>
+              <li>（3）通过任何不正当手段或以违反诚信原则的方式为自己或他人开通本服务</li>
+              <li>（4）通过非校友说指定的方式为自己或他人开通本服务</li>
+              <li>（5）通过侵犯校友说或他人合法权益的方式为自己或他人开通本服务</li>
+              <li>（6）通过其他违反相关法律、行政法规、国家政策等的方式为自己或他人开通本服务</li>
+            </ul>
+          </div>
+
+          <div class="content-title">六、使用规则</div>
+          <div class="content-detail">6.1 不得利用本服务进行任何侵犯校友说以及任何第三方的知识产权、财产权、名誉权的行为。</div>
+          <div class="content-detail">6.2 本服务是一项收费服务，以任何手段侵犯、破坏校友说收取费用的行为，校友说保佑追究行为人责任及索取赔偿的权利。</div>
+          <div class="content-detail">6.3不得利用本服务进行任何违反法律法规、政策以及公序良素、社会公德等的行为</div>
+          <div class="content-detail">
+            6.4 用户对其在平台上传、发布或者传输的的内容负全部责任，所有用户不得发布含有下列内容之一的信息，否则校友说有权自行处理并不通知用户：
+            <ul>
+              <li>（1）违反宪法确定的基本原则的</li>
+              <li>（2）危害国家安全、泄漏国家机密、颠覆国家政权、破坏国家统一的</li>
+              <li>（3）损害国家荣誉和利益的</li>
+              <li>（4）煽动民族仇恨、民族歧视、破坏民族团结的</li>
+              <li>（5）破坏国家宗教政策、宣扬邪教和封建迷信的</li>
+              <li>（6）散布谣言、扰乱社会秩序、破坏社会稳定的</li>
+              <li>（7）散布淫秽、色情、赌博、暴力、恐怖或者教唆犯最的</li>
+              <li>（8）侮辱或者诽谤他人、侵害他人合法权益的</li>
+              <li>（9）煽动非法集会、结社、游行、示威、聚众扰乱社会秩序的</li>
+              <li>（10）以非法民间组织名义活动的</li>
+              <li>（11）含有法律、行政法规禁止的其他内容</li>
+            </ul>
+          </div>
+
+          <div class="content-title">七、隐私保护</div>
+          <div class="content-detail">7.1 校友说不对外公开或向第三方提供单个用户的注册资料，但下列情况除外：
+            <ul>
+              <li>（1）事先获得用户的明确授权</li>
+              <li>（2）根据有关的法律法规要求</li>
+              <li>（3）按照相关政府主管部门的要求</li>
+              <li>（4）为维护社会公众的利益</li>
+            </ul>
+          </div>
+
+          <div class="content-title">八、版权声明</div>
+          <div class="content-detail">8.1 校友说的所有内容（包括但不限于文字、图片、音频、视频）等版权均归上海逐境网络科技有限公司享有，未经许可，不得任意转载。</div>
+
+          <div class="content-title">九、服务的中止、终止及变更</div>
+          <div class="content-detail">9.1 本服务的中止或终止包含如下情况：
+            <ul>
+              <li>（1）用户主动中止或终止的</li>
+              <li>（2）因为用户违约行为，校友说主动中止或终止服务的</li>
+              <li>（3）因国家或相关部门要求或发生不可抗力事件时，校友说中止或终止服务的</li>
+              <li>（4）其他根据法律法规应中止或终止服务的</li>
+              <li>（5）中止或终止服务后，校友说有权利但无义务确保您收到特别提示或通知</li>
+            </ul>
+          </div>
+
+          <div class="content-title">十、其他</div>
+          <div class="content-detail">10.1 您使用本服务即视为您已阅读并同意受本协议的约束。</div>
+        </div>
+        <div @click="showAgreement=false">
+          <x-icon type="ios-close-empty" size="30"></x-icon>
+        </div>
+      </x-dialog>
     </div>
     <tabbar slot="bottom">
       <div class="bottom-wrapper">
@@ -62,7 +136,7 @@
 </template>
 
 <script>
-import { ViewBox, Tabbar, Panel, Card, XButton, Alert, Loading } from "vux";
+import { ViewBox, Tabbar, Panel, Card, XButton, Alert, Loading, CheckIcon, XDialog } from "vux";
 import product from '../Product';
 
 export default {
@@ -73,27 +147,18 @@ export default {
     XButton,
     Alert,
     Loading,
+    CheckIcon,
+    XDialog,
     Card
   },
   data() {
     return {
-      list: [
-        {
-          title: "校友简历",
-          desc: ""
-        },
-        {
-          title: "求学经历",
-        },
-        {
-          title: "分享内容",
-          desc: "本次校友分享时长约1小时，分享主题为："
-        },
-      ],
       showMsg: false,
       msgTitle: '',
       msgMessage: '',
       showLoading: false,
+      agreement: false,
+      showAgreement: false
     };
   },
   beforeDestroy() {
@@ -161,11 +226,14 @@ export default {
         const { response } = err;
         if (response.status === 401) {
           const url = window.location.href;
-          window.location.href = `http://talktoalumni.com/api/login?redirect=${url}`;
+          window.location.href = `/api/login?redirect=${url}`;
         }
         throw err;
       });
     },
+    handleAgreement() {
+      this.showAgreement = true;
+    }
   }
 };
 </script>
@@ -211,6 +279,15 @@ export default {
     li {
       padding-bottom: 3px;
     }
+    .weui-icon-success {
+      font-size: 16px;
+    }
+    .weui-icon-success-circle {
+      font-size: 16px;
+    }
+    .weui-icon-circle {
+      font-size: 16px;
+    }
   }
 }
 .bottom-wrapper {
@@ -228,6 +305,19 @@ export default {
   .bottom-content-right {
     position: absolute;
     right: 6px;
+  }
+}
+.agreement-content {
+  height: 200px;
+  padding: 15px 0;
+  overflow: scroll;
+  font-size: 12px;
+  .content-detail {
+    text-align: left;
+    padding: 0 12px;
+    ul {
+      list-style: none;
+    }
   }
 }
 </style>
